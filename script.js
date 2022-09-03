ca143f5f987e95135bf9c76072c72bd08bcc5ea7
EmmaClaireOsburg <eco9492@web.de>
2022-09-03 11:48:50 +0200

    game finished

--- script.js
+++ script.js
@@ -1,21 +1,40 @@
 let fields = [];
-
+let gameOver = false
 let currentShape = 'cross';
 
 function fillShape(id) {
+    if(!fields[id] && !gameOver) {
     if(currentShape == 'cross') {
         currentShape = 'circle';
         document.getElementById('player1').classList.remove('playerInactive');
-        document.getElementById('player2').classList.add('playerInactive')
+        document.getElementById('player2').classList.add('playerInactive');
     } else {currentShape = 'cross'
         document.getElementById('player1').classList.add('playerInactive');
-        document.getElementById('player2').classList.remove('playerInactive')
+        document.getElementById('player2').classList.remove('playerInactive');
     }
 
     fields[id] = currentShape;
     console.log(fields);
     draw();
     checkForWin();
+}
+}
+
+function restart(){
+    gameOver = false;
+    fields = [];
+
+    document.getElementById('gameOver').classList.add('d-none');
+    document.getElementById('restartButton').classList.add('d-none');
+
+    for(let i = 0; i < 7; i++)
+    document.getElementById('line' + i).classList.add('d-none');
+
+    for(let i = 0; i < 9; i++){
+    document.getElementById('circle-' + i).classList.add('d-none');
+    document.getElementById('cross-' + i).classList.add('d-none');
+    }
+    
 }
 
 function draw(){
@@ -36,38 +55,51 @@ function checkForWin(){
 
     if(fields[0] == fields[1] && fields[1] == fields[2] && fields[0]) {
         winner = fields[0];
+        document.getElementById('line0').style.transform = 'scaleX(1)';
     }
 
     if(fields[3] == fields[4] && fields[4] == fields[5] && fields[3]) {
         winner = fields[3];
+        document.getElementById('line1').style.transform = 'scaleX(1)';
     }
 
     if(fields[6] == fields[7] && fields[7] == fields[8] && fields[6]) {
         winner = fields[6];
+        document.getElementById('line2').style.transform = 'scaleX(1)';
     }
 
     if(fields[0] == fields[3] && fields[3] == fields[6] && fields[0]) {
         winner = fields[0];
+        document.getElementById('line4').style.transform = 'scaleX(1) rotate(90deg)';
     }
 
     if(fields[1] == fields[4] && fields[4] == fields[7] && fields[1]) {
         winner = fields[1];
+        document.getElementById('line3').style.transform = 'scaleX(1) rotate(90deg)';
     }
 
-    if(fields[3] == fields[5] && fields[5] == fields[8] && fields[3]) {
+    if(fields[2] == fields[5] && fields[5] == fields[8] && fields[2]) {
         winner = fields[3];
+        document.getElementById('line5').style.transform = 'scaleX(1) rotate(90deg)';
     }
 
     if(fields[0] == fields[4] && fields[4] == fields[8] && fields[0]) {
         winner = fields[0];
+        document.getElementById('line6').style.transform = 'scalex(1) rotate(45deg)' ;
     }
 
     if(fields[2] == fields[4] && fields[4] == fields[6] && fields[2]) {
         winner = fields[2];
+        document.getElementById('line7').style.transform = 'scalex(1) rotate(-45deg)'
     }
 
     if(winner){
     console.log('Gewonnen', winner);
+    gameOver = true;
+    setTimeout(function(){
+        document.getElementById('restartButton').classList.remove('d-none');
+        document.getElementById('gameOver').classList.remove('d-none');
+    },1200) 
     }
 
 }
